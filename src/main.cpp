a2f3dce72f550659a1ceeff7db5b9ae5807d8de3
Connor Worley <connorbworley@gmail.com>
2017-02-22 02:11:29 -0800

    Match WPILib behavior better

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
 #include <WPILib.h>
+#include <Internal/HardwareHLReporting.h>
+#include <WPILibVersion.h>
 #include "Hardware.h"
 
 int main()
 {
+  frc::RobotState::SetImplementation(frc::DriverStation::GetInstance());
+  frc::HLUsageReporting::SetImplementation(new frc::HardwareHLReporting());
+
+  NetworkTable::SetNetworkIdentity("Robot");
+  NetworkTable::SetPersistentFilename("/home/lvuser/networktables.ini");
+
+  SmartDashboard::init();
+
+  std::FILE* file = nullptr;
+  file = std::fopen("/tmp/frc_versions/FRC_Lib_Version.ini", "w");
+
+  if(file != nullptr)
+  {
+    std::fputs("C++", file);
+    std::fputs(WPILibVersion, file);
+    std::fclose(file);
+  }
+
   auto lw = frc::LiveWindow::GetInstance();
   HAL_Report(HALUsageReporting::kResourceType_Framework,
              HALUsageReporting::kFramework_Simple);
@@ -14,7 +34,8 @@ int main()
 
   HAL_ObserveUserProgramStarting();
 
-  std::cout << "Left motor value: " << team5499::Hardware::LeftDrive1.Get() << std::endl;
+  std::cout << "Left motor value: " << team5499::Hardware::LeftDrive1.Get()
+            << std::endl;
 
   while(true)
   {
