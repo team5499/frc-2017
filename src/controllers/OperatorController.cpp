7d365a9e8b356fbb2debf02b26da869e54fe1c98
Ben Scholar <bens@digitalman.com>
2017-10-14 18:33:55 -0700

    Flashing LEDs on climb

--- src/controllers/OperatorController.cpp
+++ src/controllers/OperatorController.cpp
@@ -4,10 +4,12 @@ namespace team5499
 {
   void OperatorController::Start()
   {
+    timer.Start();
+    flash = false;
   }
   void OperatorController::Handle()
   {
-    
+    millis = timer.Get() * 1000;
     //Drivetrain
     subsystems::drivetrain.Drive(subsystems::joysticks.getLeftValue(), subsystems::joysticks.getRightValue());
     //Gearmech
@@ -17,7 +19,22 @@ namespace team5499
     if(subsystems::gearmech.GearDetected())
       subsystems::led.flash(100, 100, 100);
     //Climber
-    subsystems::climber.SetClimber(subsystems::joysticks.getClimberValue());
+    if(subsystems::joysticks.getClimberValue() == 1.0) {
+      subsystems::climber.SetClimber(subsystems::joysticks.getClimberValue());
+      flashLEDs();
+    }
 
+  }
+
+  void OperatorController::flashLEDS() {
+    if(millis % 150 == 0) {
+      if(flash) {
+        subsystems::led.setRGB(255, 0, 0);
+        flash = !flash;
+      } else {
+        subsystems::led.setRGB(0, 255, 0);
+        flash = !flash;
+      }
+    }
   }
 }
--- src/controllers/OperatorController.h
+++ src/controllers/OperatorController.h
@@ -7,8 +7,13 @@ namespace team5499
 {
   class OperatorController : public GenericController
   {
+  private:
+    Timer timer;
+    int millis;
+    bool flash;
   public:
     void Start() override;
     void Handle() override;
+    void flashLEDs();
   };
 }
